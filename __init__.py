56ebabc282591657415d0060972d4aa474e6e1ed
huboqiang <huboqiang@gmail.com>
2015-06-24 03:06:07 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+
+
+
+__author__ = "John Boqiang Hu"
+__copyright__ = "Copyright 2015, Peking University."
+__credits__ = [ ]
+__license__ = "GPL"
+__version__ = "2.0.1"
+__maintainer__ = "John Boqiang Hu"
+__email__ = "huboqiang@gmail.com"
+__status__ = "Production"
+__all__ = ["frame", "settings", "utils"]
